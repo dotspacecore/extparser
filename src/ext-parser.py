2ad0b955dae333e6265e497d4b9845e8996216bb
alesandy <smdreg@gmail.com>
2020-01-05 20:37:40 -0300

    Refactor comments

--- src/ext-parser.py
+++ src/ext-parser.py
@@ -5,6 +5,7 @@ import sqlite3 as sqldb
 now = datetime.datetime.now()
 strNowTime = now.strftime('%d%m%Y_%H%M%S')
 
+# initialize local db to obtain required data for YNAB
 conn = sqldb.connect('db/ynablocal.db')
 sqlSelect = '''
 SELECT a.nombre AS 'acreedor', c.nombre AS 'categoria', sc.nombre AS 'subcategoria', p.descripcion AS 'palabraclave' 
@@ -15,6 +16,7 @@ JOIN categoria c ON sc.categoria=c.id
 WHERE p.descripcion = ?
 '''
 
+# for now files are deposited on fixed path locations
 inputFilePath = './in/extracto-cuenta.txt'
 outputFilePath = f'./out/extracto-csv-{strNowTime}.csv'
 
@@ -22,30 +24,30 @@ file = open(inputFilePath)
 filelines = file.readlines()
 print(f'lines array length {len(filelines)}')
 movs = filelines[28:-10]
-# crear el archivo csv y agregar la cabecera
+# create csv file and add header row
 csvfile = open(outputFilePath, 'w')
 csvfile.write("Date,Payee,Category,Memo,Outflow,Inflow\n")
 
 for line in movs:
     if line.strip():
         # print(line.strip())
-        # reemplazar los espacios múltiples por un separador y dividir
+        # using a regex to clear whitespace and organize data
         newline = re.sub(' {2,}', ';', line.rstrip().strip())
         try:
-            # verificar si la línea empieza con un número (parte de la fecha de transacción)
-            # para filtrar el pie de página insertado en el txt en caso de ser multipágina
+            # ignore rows that don't start with a number value
+            # doing this validation because the txt adds a page foot in case there are enough rows
             int(newline[0:2])
             #print(newline)
             aux = newline.split(';')
-            # desempaquetar los datos relevantes del arreglo en distintas variables
+            # unpack the data needed
             fecha, _, _, descripcion, importe_debito, importe_credito, _, _ = aux
-            # sacar el separador de miles de los montos
+            # delete any comma separator from monetary variables
             debito = importe_debito.replace(',', '')
             credito = importe_credito.replace(',', '')
             #print(f'{fecha}, {descripcion}, {debito}, {credito}')
             acreedor = categoria = subcategoria = ''
 
-            # obtener el acreedor y la categoría desde una bd local
+            # connect to local database and query remaining required data
             with conn:
                 cursor = conn.cursor()
                 cursor.execute(sqlSelect, (descripcion,))
@@ -54,10 +56,9 @@ for line in movs:
                     #print(row)
                     acreedor, categoria, subcategoria, _ = row
 
-            # armar la cadena para el archivo csv
+            # format the csv row to insert on its file
             csvstr = f"{fecha},{acreedor},{categoria}: {subcategoria},{descripcion},{debito},{credito}\n"
             print(csvstr)
-            # agregar la línea al archivo csv
             csvfile.write(csvstr)
 
         except ValueError:
